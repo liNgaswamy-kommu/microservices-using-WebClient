f0169482df642fc6d55210f76ce9f9d6e3b8d5e4
Lingaswamy Kommu <lingaswamykommu95@gmail.com>
2026-09-18 14:06:31 +0530

    WebClient + after adding Eureka server

--- order-service-wc/src/main/java/com/eidko/order_service_wc/client/ProductClient.java
+++ order-service-wc/src/main/java/com/eidko/order_service_wc/client/ProductClient.java
@@ -1,15 +1,19 @@
 package com.eidko.order_service_wc.client;
 
 import com.eidko.order_service_wc.dto.ProductResponseDTO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
 public class ProductClient {
 
-    @Autowired
-    private WebClient webClient;
+    private final WebClient webClient;
+
+    public ProductClient(WebClient.Builder builder){
+        this.webClient = builder
+                .baseUrl("http://product-service")
+                .build();
+    }
 
     public ProductResponseDTO getProductById(Long id){
         return webClient.get()
--- order-service-wc/src/main/java/com/eidko/order_service_wc/config/WebClientConfig.java
+++ order-service-wc/src/main/java/com/eidko/order_service_wc/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.eidko.order_service_wc.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,10 +9,8 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     @Bean
-    public WebClient webClient(){
-
-        return WebClient.builder()
-                .baseUrl("http://localhost:9091")
-                .build();
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder(){
+        return WebClient.builder();
     }
 }
